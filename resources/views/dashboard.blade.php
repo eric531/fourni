468f1be0d683c693bd685ab46115d2d685716af5
eric531 <erictohe@gmail.com>
2023-11-09 18:10:38 +0000

    ajout des vues

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,238 +2,135 @@
 
 
 @section('content')
-<style>
-        /* Styles pour la carte */
-        .card {
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            max-width: 400px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: #f9f9f9; /* Couleur de fond de la carte */
-        }
 
-        /* Styles pour les éléments du formulaire */
-        .card p {
-            margin: 10px 0;
-        }
-
-        .card strong {
-            font-weight: bold;
-        }
-
-        /* Styles pour le bouton */
-        .card button {
-            background-color: #007BFF;
-            color: #fff;
-            border: none;
-            padding: 10px 20px;
-            cursor: pointer;
-            border-radius: 5px;
-        }
-    </style>
-<div id="page-wrapper" style=>
-		<div class="main-page">
-
-			<div class="row" style="padding-top:15px;background:#ddd;  margin-bottom:20px; margin-top:50px;">
-
-
-			</div>
-
-
-			<div class="row" style="background:#ddd; margin-bottom:20px;">
-
-				<br>
-				<form method="GET" action="{{route('recherche')}}">
-
-					@csrf
-					<div class="col-md-6">
-						<center style="font-size:18px; text-align:center;padding-top:3px;">
-						Rechercher un fournisseur agrée
-						</center>
+	<!-- main content start-->
+    <div id="page-wrapper">
+			<div class="main-page">
+				<div class="row-one">
+					<div class="col-md-4 widget">
+						<div class="stats-left ">
+							<h5>NOMBRE</h5>
+							<h4>FOURNISSEURS AGREES</h4>
+						</div>
+						<div class="stats-right">
+							<label> 45</label>
+						</div>
+						<div class="clearfix"> </div>	
 					</div>
-
-					<div class="col-md-3">
-						<div class="form-group">
-							 <input type="text" name="code_fournisseur" class="form-control" id="exampleInputEmail3" placeholder="code: FCIxxxxx0">
+					<div class="col-md-4 widget states-mdl">
+						<div class="stats-left">
+							<h5>NOMBRE</h5>
+							<h4>FOURNISSEURS PROSPECTS</h4>
+						</div>
+						<div class="stats-right">
+							<label> 80</label>
+						</div>
+						<div class="clearfix"> </div>	
+					</div>
+					<div class="col-md-4 widget states-last">
+						<div class="stats-left">
+							<h5>NOMBRE DE</h5>
+							<h4>FOURNISSEURS BLACKLISTES</h4>
+						</div>
+						<div class="stats-right">
+							<label>51</label>
+						</div>
+						<div class="clearfix"> </div>	
+					</div>
+					<div class="clearfix"> </div>	
+				</div>
+				
+				
+				<div class="row">
+					<div class="col-md-4 stats-info widget">
+						<div class="stats-title">
+							<h4 class="title">Browser Stats</h4>
+						</div>
+						<div class="stats-body">
+							<ul class="list-unstyled">
+								<li>GoogleChrome <span class="pull-right">85%</span>  
+									<div class="progress progress-striped active progress-right">
+										<div class="bar green" style="width:85%;"></div> 
+									</div>
+								</li>
+								<li>Firefox <span class="pull-right">35%</span>  
+									<div class="progress progress-striped active progress-right">
+										<div class="bar yellow" style="width:35%;"></div>
+									</div>
+								</li>
+								<li>Internet Explorer <span class="pull-right">78%</span>  
+									<div class="progress progress-striped active progress-right">
+										<div class="bar red" style="width:78%;"></div>
+									</div>
+								</li>
+								<li>Safari <span class="pull-right">50%</span>  
+									<div class="progress progress-striped active progress-right">
+										<div class="bar blue" style="width:50%;"></div>
+									</div>
+								</li>
+								<li>Opera <span class="pull-right">80%</span>  
+									<div class="progress progress-striped active progress-right">
+										<div class="bar light-blue" style="width:80%;"></div>
+									</div>
+								</li>
+								<li class="last">Others <span class="pull-right">60%</span>  
+									<div class="progress progress-striped active progress-right">
+										<div class="bar orange" style="width:60%;"></div>
+									</div>
+								</li> 
+							</ul>
 						</div>
 					</div>
-
-					<div class="col-md-3">
-
-						<div class="form-group">
-							<button type="submit" class="form-group">Rechercher</button>
-						</div>
-
+					<div class="col-md-8 stats-info stats-last widget-shadow">
+						<table class="table stats-table ">
+							<thead>
+								<tr>
+									<th>S.NO</th>
+									<th>PRODUCT</th>
+									<th>STATUS</th>
+									<th>PROGRESS</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<th scope="row">1</th>
+									<td>Lorem ipsum</td>
+									<td><span class="label label-success">In progress</span></td>
+									<td><h5>85% <i class="fa fa-level-up"></i></h5></td>
+								</tr>
+								<tr>
+									<th scope="row">2</th>
+									<td>Aliquam</td>
+									<td><span class="label label-warning">New</span></td>
+									<td><h5>35% <i class="fa fa-level-up"></i></h5></td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>Lorem ipsum</td>
+									<td><span class="label label-danger">Overdue</span></td>
+									<td><h5  class="down">40% <i class="fa fa-level-down"></i></h5></td>
+								</tr>
+								<tr>
+									<th scope="row">4</th>
+									<td>Aliquam</td>
+									<td><span class="label label-info">Out of stock</span></td>
+									<td><h5>100% <i class="fa fa-level-up"></i></h5></td>
+								</tr>
+								<tr>
+									<th scope="row">5</th>
+									<td>Lorem ipsum</td>
+									<td><span class="label label-success">In progress</span></td>
+									<td><h5 class="down">10% <i class="fa fa-level-down"></i></h5></td>
+								</tr>
+								<tr>
+									<th scope="row">6</th>
+									<td>Aliquam</td>
+									<td><span class="label label-warning">New</span></td>
+									<td><h5>38% <i class="fa fa-level-up"></i></h5></td>
+								</tr>
+							</tbody>
+						</table>
 					</div>
-
-				</form>
-
-			</div>
-
-
-
-		</div>
-
-		<div class="container">
-
-			@if(session('error'))
-				<div style=" color: red;">{{ session('error') }}</div>
-			@endif
-
-			@if(session('success'))
-				<div style=" color: green;">{{ session('success') }}</div>
-			@endif
-
-			<h1>Détails du fournisseur</h1>
-
-			@if (isset($searchfournisseur) && !session('error'))
-				<h3>Ajouter un fournisseur</h3>
-
-				<!-- <form action="{{route('ajouterfournisseurs')}}" method="POST">
-					@csrf
-
-					<p>
-						<strong>Nom du fournisseur:</strong> {{ isset($searchfournisseur['entreprise']) ? $searchfournisseur['entreprise'] : 'Non disponible' }}
-					</p>
-					<p>
-						<strong>Domaine du fournisseur:</strong> {{ isset($searchfournisseur['domaine_activites_1']) ? $searchfournisseur['domaine_activites_1'] : 'Non disponible' }}
-					</p>
-					<p>
-						<strong>Gmail du fournisseur:</strong> {{ isset($searchfournisseur['email']) ? $searchfournisseur['email'] : 'Non disponible' }}
-					</p>
-                    <p>
-						<strong>Mobile:</strong> {{ isset($searchfournisseur['mobile']) ? $searchfournisseur['mobile'] : 'Non disponible' }}
-					</p>
-                    <p>
-						<strong>Fixe:</strong> {{ isset($searchfournisseur['fixe']) ? $searchfournisseur['fixe'] : 'Non disponible' }}
-					</p>
-                    <p>
-						<strong>rccm:</strong> {{ isset($searchfournisseur['mobilrccme']) ? $searchfournisseur['rccm'] : 'Non disponible' }}
-					</p>
-                    <p>
-						<strong>cc:</strong> {{ isset($searchfournisseur['cc']) ? $searchfournisseur['cc'] : 'Non disponible' }}
-					</p>
-                    <p>
-						<strong>date_dfe:</strong> {{ isset($searchfournisseur['date_dfe']) ? $searchfournisseur['date_dfe'] : 'Non disponible' }}
-					</p>
-                    <p>
-						<strong>situation_geo:</strong> {{ isset($searchfournisseur['situation_geo']) ? $searchfournisseur['situation_geo'] : 'Non disponible' }}
-					</p>
-                    <p>
-						<strong>sous_domaine:</strong> {{ isset($searchfournisseur['sous_domaine']) ? $searchfournisseur['sous_domaine'] : 'Non disponible' }}
-					</p>
-                    <p>
-						<strong>produits_services:</strong> {{ isset($produits_services->produits_services) ? $produits_services->produits_services : 'Non disponible' }}
-					</p>
-                    <p>
-						<strong>validite_arf:</strong> {{ isset($searchfournisseur['validite_arf']) ? $searchfournisseur['validite_arf'] : 'Non disponible' }}
-					</p>
-                    <p>
-						<strong>validite_cnps:</strong> {{ isset($searchfournisseur['validite_cnps']) ? $searchfournisseur['validite_cnps'] : 'Non disponible' }}
-					</p>
-                    <p>
-						<strong>interlocuteur:</strong>
-                        <p>{{$interlocuteur->interloc_nom}}</p>
-                        <p>{{$interlocuteur->interloc_fonction}}</p>
-                        <p>{{$interlocuteur->interloc_contact}}</p>
-                        <p>{{$interlocuteur->interloc_email}}</p>
-
-					</p>
-                    <p>
-						<strong>status:</strong> {{ $searchfournisseur['status']==1? 'Vrai': 'Faux'  }}
-					</p>
-
-					@if (isset($searchfournisseur['id']))
-						<input type="hidden" name="fournisseur" value="{{ $searchfournisseur['id'] }}">
-					@else
-						<input type="hidden" name="fournisseur" value="">
-					@endif
-
-					<button type="submit" style="background-color: #007BFF; color: #fff; border: none; padding: 10px 20px; cursor: pointer;">
-							Ajouter fournisseur
-					</button>
-
-
-				</form> -->
-
-                <div class="card">
-        <form action="{{ route('ajouterfournisseurs') }}" method="POST">
-            @csrf
-
-            <p>
-                <strong>Nom du fournisseur:</strong> {{ isset($searchfournisseur['entreprise']) ? $searchfournisseur['entreprise'] : 'Non disponible' }}
-            </p>
-            <p>
-                <strong>Domaine du fournisseur:</strong> {{ isset($searchfournisseur['domaine_activites_1']) ? $searchfournisseur['domaine_activites_1'] : 'Non disponible' }}
-            </p>
-            <p>
-                <strong>Gmail du fournisseur:</strong> {{ isset($searchfournisseur['email']) ? $searchfournisseur['email'] : 'Non disponible' }}
-            </p>
-            <p>
-                <strong>Mobile:</strong> {{ isset($searchfournisseur['mobile']) ? $searchfournisseur['mobile'] : 'Non disponible' }}
-            </p>
-            <p>
-                <strong>Fixe:</strong> {{ isset($searchfournisseur['fixe']) ? $searchfournisseur['fixe'] : 'Non disponible' }}
-            </p>
-            <p>
-                <strong>rccm:</strong> {{ isset($searchfournisseur['rccm']) ? $searchfournisseur['rccm'] : 'Non disponible' }}
-            </p>
-            <p>
-                <strong>cc:</strong> {{ isset($searchfournisseur['cc']) ? $searchfournisseur['cc'] : 'Non disponible' }}
-            </p>
-            <p>
-                <strong>date_dfe:</strong> {{ isset($searchfournisseur['date_dfe']) ? $searchfournisseur['date_dfe'] : 'Non disponible' }}
-            </p>
-            <p>
-                <strong>situation_geo:</strong> {{ isset($searchfournisseur['situation_geo']) ? $searchfournisseur['situation_geo'] : 'Non disponible' }}
-            </p>
-            <p>
-                <strong>sous_domaine:</strong> {{ isset($searchfournisseur['sous_domaine']) ? $searchfournisseur['sous_domaine'] : 'Non disponible' }}
-            </p>
-            <p>
-                <strong>produits_services:</strong> {{ isset($produits_services->produits_services) ? $produits_services->produits_services : 'Non disponible' }}
-            </p>
-            <p>
-                <strong>validite_arf:</strong> {{ isset($searchfournisseur['validite_arf']) ? $searchfournisseur['validite_arf'] : 'Non disponible' }}
-            </p>
-            <p>
-                <strong>validite_cnps:</strong> {{ isset($searchfournisseur['validite_cnps']) ? $searchfournisseur['validite_cnps'] : 'Non disponible' }}
-            </p>
-            <p>
-                <strong>interlocuteur:</strong>
-                <p>{{ $interlocuteur->interloc_nom }}</p>
-                <p>{{ $interlocuteur->interloc_fonction }}</p>
-                <p>{{ $interlocuteur->interloc_contact }}</p>
-                <p>{{ $interlocuteur->interloc_email }}</p>
-            </p>
-            <p>
-                <strong>status:</strong> {{ $searchfournisseur['status'] == 1 ? 'Vrai' : 'Faux' }}
-            </p>
-
-            @if (isset($searchfournisseur['id']))
-                <input type="hidden" name="fournisseur" value="{{ $searchfournisseur['id'] }}">
-            @else
-                <input type="hidden" name="fournisseur" value="">
-            @endif
-
-            <button type="submit">
-                Ajouter fournisseur
-            </button>
-        </form>
-    </div>
-			@endif
-
-			@if (!isset($searchfournisseur) && !session('success') && !session('error'))
-				<p>Le fournisseur n'a pas été trouvé.</p>
-			@endif
-
-
-
-		</div>
-
-
-
-		</div>
+					<div class="clearfix"> </div>
+				</div>
 @endsection
